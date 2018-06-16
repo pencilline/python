3c9c9d0bbe98141e9a556b0d2b1289b49bb7560e
pencilline <40315114+pencilline@users.noreply.github.com>
2018-06-16 09:20:53 +0000

    Add files via upload
    Automate The Boring Stuff vampire.py

---
+++ vampire.py
@@ -0,0 +1,13 @@
+print('Hello, please enter passphrase:')
+Myname = input()
+print('Welcome, + Myname')
+print('Please enter your age')
+age = input()
+if Myname == 'Alice':
+    print('Hi, Alice.')
+elif age > 12:
+    print('You are not Alice, kiddo.')
+elif age > 2000:
+    print('Unlike you, ALice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
